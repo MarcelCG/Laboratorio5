b256d255b20065dd414366025cb4c0918a3fb582
MarcelCG <marcel.castro@ucr.ac.cr>
2023-09-30 19:17:01 -0600

    Laboratorio6 finalizado

--- Laboratorio5/Handlers/PaisesHandler.cs
+++ Laboratorio5/Handlers/PaisesHandler.cs
@@ -53,5 +53,32 @@ namespace Laboratorio5.Handlers
             conexion.Close();
             return exito;
         }
+        public bool EditarPais(PaisModel pais)
+        {
+            var consulta = @"UPDATE [dbo].[Pais] SET Nombre = @Nombre,
+            Idioma =@Idioma,
+            Continente = @Continente
+            WHERE Id=@Id ";
+            var comandoParaConsulta = new SqlCommand(consulta, conexion);
+
+            comandoParaConsulta.Parameters.AddWithValue("@Nombre", pais.Nombre);
+            comandoParaConsulta.Parameters.AddWithValue("@Idioma", pais.Idioma);
+            comandoParaConsulta.Parameters.AddWithValue("@Continente", pais.Continente);
+            comandoParaConsulta.Parameters.AddWithValue("@Id", pais.Id);
+            conexion.Open();
+            bool exito = comandoParaConsulta.ExecuteNonQuery() >= 1;
+            conexion.Close();
+            return exito;
+        }
+        public bool BorrarPais(int identificador)
+        {
+            var consulta = @"DELETE  [dbo].[Pais]  WHERE  Id=@Id ";
+            var comandoParaConsulta = new SqlCommand(consulta, conexion);
+            comandoParaConsulta.Parameters.AddWithValue("@Id", identificador);
+            conexion.Open();
+            bool exito = comandoParaConsulta.ExecuteNonQuery() >= 1;
+            conexion.Close();
+            return exito;
+        }
     }
 }
